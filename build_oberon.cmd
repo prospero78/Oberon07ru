96cfce56a22df23d2b2f61b903526fb17ce4653d
prospero78su <prospero.78.su@gmail.com>
2016-12-07 16:36:51 +0200

    70 Вроде работает...

--- build_oberon.cmd
+++ build_oberon.cmd
@@ -1,2 +1,8 @@
 @echo off
+cls
+chcp 866
 Compiler.exe .\Source\Compiler\Compiler.ob07 con 4
+echo ===================================
+chcp 1251
+Compiler.exe .\Source\Compiler\Compiler.ob07 con 4
+chcp 866
