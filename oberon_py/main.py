47b0113c91031628463dbe23389deac8cf84d23e
prospero78su <prospero.78.su@gmail.com>
2017-05-18 22:08:32 +0200

    315 Вариант на python

--- oberon_py/main.py
+++ oberon_py/main.py
@@ -4,8 +4,13 @@
 Лицензия BSD-2.
 '''
 
+import pakOberon.modUtil as mUtil
+import pakOberon.modError as mErr
+import pakOberon.modErrorConstante as mErrConst
+
 def Start():
-    pass
+    if mUtil.param_all<2:
+        mErr.Msg(mErrConst.msgParamFew)
 
 if __name__=='__main__':
     Start()
